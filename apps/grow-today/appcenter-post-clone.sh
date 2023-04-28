237bf288805c077591b33e1bcd400e6be18fd542
Quan Do <quan.do@asnet.com.vn>
2023-04-28 10:18:02 +0700

    Remove unuse file

--- apps/grow-today/appcenter-post-clone.sh
+++
@@ -1,3 +0,0 @@
-# Creates an .env from ENV variables
-ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
-set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
--- apps/grow-today/appcenter-pre-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "PRE BUILD SCRIPTS !!!"
-echo $APPCENTER_BRANCH
-echo $APPCENTER_XCODE_PROJECT
-echo $APPCENTER_SOURCE_DIRECTORY
-echo $APPCENTER_REACTNATIVE_PACKAGE
-
-yarn install
-ls -a
