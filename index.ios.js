4853dea6797b5fe6eceae5f2036cfe0b6a9d6b01
Quan Do <quan.do@asnet.com.vn>
2023-04-28 08:55:43 +0700

    Update config entry file

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+ import {AppRegistry} from 'react-native';
+ import App from './apps/grow-today/src/app/App';
+ import {name as appName} from './apps/grow-today/app.json';
+ 
+ AppRegistry.registerComponent(appName, () => App);
+ 
