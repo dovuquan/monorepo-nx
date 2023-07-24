d2a1ef9a481eb388bf02ec4951796d0ac50ae254
Quan Do <quan.do@asnet.com.vn>
2023-07-24 15:07:56 +0700

    Update config watch folder

--- apps/mobile-app/metro.config.js
+++ apps/mobile-app/metro.config.js
@@ -31,7 +31,7 @@ module.exports = (async () => {
       // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
       extensions: [],
       // the project root to start the metro server
-      projectRoot: __dirname,
+      projectRoot: path.resolve(__dirname),
       // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
       watchFolders: [
         path.resolve(__dirname, "../../libs/ui-heading") 
