ec2a79b3f462e6e4f0b7e6c6795265f674e4a714
Quan Do <quan.do@asnet.com.vn>
2023-07-25 14:35:14 +0700

    Update project root

--- apps/mobile-app/metro.config.js
+++ apps/mobile-app/metro.config.js
@@ -2,11 +2,13 @@ const { withNxMetro } = require("@nrwl/react-native");
 const { getDefaultConfig } = require("metro-config");
 const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-const extraNodeModules = {
-  '@package/ui-heading': path.resolve(__dirname + '../../libs/ui-heading'),
-};
-
 const path = require('path');
+
+const extraNodeModules = {
+  '@package/ui-heading': path.resolve(path.join(__dirname, '../../libs/ui-heading'))
+};
+const nodeModulesPaths = [path.resolve(path.join(__dirname, './node_modules'))];
+
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
@@ -24,6 +26,7 @@ module.exports = (async () => {
       },
       resolver: {
         extraNodeModules,
+        nodeModulesPaths,
         assetExts: assetExts.filter((ext) => ext !== "svg"),
         sourceExts: [...sourceExts, "svg"],
         blockList: exclusionList([/^(?!.*node_modules).*\/dist\/.*/]),
