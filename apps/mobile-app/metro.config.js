63aa05d3f8d4bc21a070bb00fa0a52b5ad065bd6
Quan Do <quan.do@asnet.com.vn>
2023-07-25 14:58:28 +0700

    Update project root

--- apps/mobile-app/metro.config.js
+++ apps/mobile-app/metro.config.js
@@ -42,7 +42,8 @@ module.exports = (async () => {
       projectRoot: path.resolve(__dirname),
       // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
       watchFolders: [
-        path.resolve(__dirname, "../../libs/ui-heading"),
+        path.resolve(path.join(__dirname, '../../libs/ui-heading'))
+        // path.resolve(__dirname, "../../libs/ui-heading"),
       ],
     }
   );
