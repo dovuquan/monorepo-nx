b1545d3360c0d14488d2ffdc9058bdca58de9aac
Quan Do <quan.do@asnet.com.vn>
2023-07-26 08:33:13 +0700

    Update project root

--- apps/mobile-app/metro.config.js
+++ apps/mobile-app/metro.config.js
@@ -3,6 +3,14 @@ const { getDefaultConfig } = require("metro-config");
 const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const monorepoPackages = {
+  '@package/ui-heading': path.resolve(workspaceRoot, 'libs/ui-heading'),
+};
+
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
@@ -19,6 +27,7 @@ module.exports = (async () => {
         babelTransformerPath: require.resolve("react-native-svg-transformer"),
       },
       resolver: {
+        extraNodeModules: monorepoPackages,
         assetExts: assetExts.filter((ext) => ext !== "svg"),
         sourceExts: [...sourceExts, "svg"],
         blockList: exclusionList([/^(?!.*node_modules).*\/dist\/.*/]),
@@ -31,7 +40,7 @@ module.exports = (async () => {
       // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
       extensions: [],
       // the project root to start the metro server
-      projectRoot: path.resolve(__dirname),
+      projectRoot: workspaceRoot,
       // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
       watchFolders: [
         path.resolve(__dirname, "../../libs/ui-heading"),
